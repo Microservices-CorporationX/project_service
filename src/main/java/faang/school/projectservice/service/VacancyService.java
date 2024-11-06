3c7e37c97d846a6d126d3aa16d96e0320c0a5467
Andrew Balashov <balashov83@gmail.com>
2024-11-06 12:12:22 +0300

    BJS2-40651 Feature: Create Vacancy - create full vacancy from dto
    VacancyService: toEntityFromDto method added (add fields ignored by mapper)
    ProjectService: getProjectById metod added

--- src/main/java/faang/school/projectservice/service/VacancyService.java
+++ src/main/java/faang/school/projectservice/service/VacancyService.java
@@ -2,6 +2,7 @@ package faang.school.projectservice.service;
 
 import faang.school.projectservice.dto.client.VacancyDto;
 import faang.school.projectservice.mapper.VacancyMapper;
+import faang.school.projectservice.model.Vacancy;
 import faang.school.projectservice.repository.VacancyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,15 @@ import org.springframework.stereotype.Service;
 public class VacancyService {
     private final VacancyRepository vacancyRepository;
     private final VacancyMapper vacancyMapper;
+    private final ProjectService projectService;
 
     public VacancyDto create(VacancyDto vacancyDto) {
         return vacancyDto;
+    }
+
+    public Vacancy toEntityFromDto(VacancyDto dto) {
+        Vacancy vacancy = vacancyMapper.toEntity(dto);
+        vacancy.setProject(projectService.getProjectById(dto.getProjectId()));
+        return vacancy;
     }
 }
