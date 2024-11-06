7aa5de363fa40d3fad06c316ebe60b46b367e127
Andrew Balashov <balashov83@gmail.com>
2024-11-07 00:07:53 +0300

    BJS2-40651 Feature: Update Vacancy - validator update
    VacancyValidator: checks the role od updater and the number of candidates for vacancy closure
    Exception: InsufficientCandidatesException added

--- src/main/java/faang/school/projectservice/validator/vacancy/VacancyValidator.java
+++ src/main/java/faang/school/projectservice/validator/vacancy/VacancyValidator.java
@@ -1,9 +1,11 @@
 package faang.school.projectservice.validator.vacancy;
 
-import faang.school.projectservice.dto.client.VacancyDto;
+import faang.school.projectservice.dto.vacancy.VacancyDto;
 import faang.school.projectservice.exception.DataValidationException;
+import faang.school.projectservice.exception.InsufficientCandidatesException;
 import faang.school.projectservice.model.TeamMember;
 import faang.school.projectservice.model.TeamRole;
+import faang.school.projectservice.model.Vacancy;
 import faang.school.projectservice.service.ProjectService;
 import faang.school.projectservice.service.TeamMemberService;
 import jakarta.persistence.EntityNotFoundException;
@@ -15,7 +17,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class VacancyValidator {
-    private static final List<TeamRole> ROLES_TO_CREATE_VACANCY = List.of(TeamRole.OWNER, TeamRole.MANAGER);
+    private static final List<TeamRole> ROLES_TO_MANAGE_VACANCY = List.of(TeamRole.OWNER, TeamRole.MANAGER);
     private final ProjectService projectService;
     private final TeamMemberService teamMemberService;
 
@@ -26,9 +28,23 @@ public class VacancyValidator {
     }
 
     public void validateVacancyCreatorRole(VacancyDto dto) {
-        TeamMember teamMember = teamMemberService.getTeamMemberById(dto.getCreatedBy());
-        if(teamMember.getRoles().stream().noneMatch((ROLES_TO_CREATE_VACANCY::contains))) {
-            throw new DataValidationException("Vacancy can be created by following roles " + ROLES_TO_CREATE_VACANCY);
+        TeamMember creator = teamMemberService.getTeamMemberById(dto.getCreatedBy());
+        if (creator.getRoles().stream().noneMatch((ROLES_TO_MANAGE_VACANCY::contains))) {
+            throw new DataValidationException("Vacancy can be created by following roles " + ROLES_TO_MANAGE_VACANCY);
+        }
+    }
+
+    public void validateVacancyUpdaterRole(VacancyDto dto) {
+        TeamMember updater = teamMemberService.getTeamMemberById(dto.getUpdatedBy());
+        if (updater.getRoles().stream().noneMatch((ROLES_TO_MANAGE_VACANCY::contains))) {
+            throw new DataValidationException("Vacancy status can be modified by following roles " + ROLES_TO_MANAGE_VACANCY);
+        }
+    }
+
+    public void validateCandidateCountForClosure(Vacancy vacancy) {
+        if (vacancy.getCandidates().size() < vacancy.getCount()) {
+            throw new InsufficientCandidatesException(
+                    "Vacancy should have at least " + vacancy.getCount() + " candidates to be closed");
         }
     }
 }
--- src/test/java/faang/school/projectservice/validator/VacancyValidatorTest.java
+++ src/test/java/faang/school/projectservice/validator/VacancyValidatorTest.java
@@ -1,10 +1,9 @@
 package faang.school.projectservice.validator;
 
-import faang.school.projectservice.dto.client.VacancyDto;
+import faang.school.projectservice.dto.vacancy.VacancyDto;
 import faang.school.projectservice.exception.DataValidationException;
-import faang.school.projectservice.model.TeamMember;
-import faang.school.projectservice.model.TeamRole;
-import faang.school.projectservice.model.WorkSchedule;
+import faang.school.projectservice.exception.InsufficientCandidatesException;
+import faang.school.projectservice.model.*;
 import faang.school.projectservice.service.ProjectService;
 import faang.school.projectservice.service.TeamMemberService;
 import faang.school.projectservice.validator.vacancy.VacancyValidator;
@@ -37,16 +36,18 @@ class VacancyValidatorTest {
 
     VacancyDto dto;
     TeamMember teamMember;
+    Vacancy vacancy;
 
     @BeforeEach
     void setUp() {
         dto = createTestVacancyDto();
         teamMember = createTestTeamMember();
+        vacancy = createTestVacancy();
     }
 
     @Test
     @DisplayName("Check project in vacancy exists")
-    void validateProjectInVacancyExists() {
+    void testValidateProjectInVacancyExists() {
         when(projectService.checkProjectExistsById(dto.getProjectId())).thenReturn(true);
 
         assertDoesNotThrow(() -> vacancyValidator.validateProjectInVacancyExists(dto));
@@ -56,7 +57,7 @@ class VacancyValidatorTest {
 
     @Test
     @DisplayName("Check project in vacancy doesn't exist")
-    void validateProjectInVacancyNotExists() {
+    void testValidateProjectInVacancyNotExists() {
         when(projectService.checkProjectExistsById(dto.getProjectId())).thenReturn(false);
 
         Exception ex = assertThrows(EntityNotFoundException.class, () -> vacancyValidator.validateProjectInVacancyExists(dto));
@@ -67,7 +68,7 @@ class VacancyValidatorTest {
 
     @Test
     @DisplayName("Check the vacancy creator has valid role")
-    void validateVacancyCreatorRoleValid() {
+    void testValidateVacancyCreatorRoleValid() {
         when(teamMemberService.getTeamMemberById(dto.getCreatedBy())).thenReturn(teamMember);
 
         assertDoesNotThrow(() -> vacancyValidator.validateVacancyCreatorRole(dto));
@@ -77,13 +78,62 @@ class VacancyValidatorTest {
 
     @Test
     @DisplayName("Check the vacancy creator has invalid role")
-    void validateVacancyCreatorRoleInvalid() {
+    void testValidateVacancyCreatorRoleInvalid() {
         teamMember.setRoles(List.of(TeamRole.DESIGNER));
         when(teamMemberService.getTeamMemberById(dto.getCreatedBy())).thenReturn(teamMember);
 
         Exception ex = assertThrows(DataValidationException.class, (() -> vacancyValidator.validateVacancyCreatorRole(dto)));
         assertEquals("Vacancy can be created by following roles " + List.of(TeamRole.OWNER, TeamRole.MANAGER), ex.getMessage());
         verify(teamMemberService, times(1)).getTeamMemberById(dto.getCreatedBy());
+    }
+
+    @Test
+    @DisplayName("Check the updater of the vacancy has valid role")
+    void testValidateVacancyUpdaterRoleValid(){
+        when(teamMemberService.getTeamMemberById(dto.getUpdatedBy())).thenReturn(teamMember);
+
+        assertDoesNotThrow(() -> vacancyValidator.validateVacancyUpdaterRole(dto));
+        verify(teamMemberService, times(1)).getTeamMemberById(dto.getUpdatedBy());
+    }
+
+    @Test
+    @DisplayName("Check the updater of the vacancy has invalid role")
+    void testValidateVacancyUpdaterRoleInvalid(){
+        teamMember.setRoles(List.of(TeamRole.ANALYST));
+        when(teamMemberService.getTeamMemberById(dto.getUpdatedBy())).thenReturn(teamMember);
+
+        Exception ex = assertThrows(DataValidationException.class, () -> vacancyValidator.validateVacancyUpdaterRole(dto));
+        assertEquals("Vacancy status can be modified by following roles " + List.of(TeamRole.OWNER, TeamRole.MANAGER), ex.getMessage());
+        verify(teamMemberService, times(1)).getTeamMemberById(dto.getUpdatedBy());
+    }
+
+    @Test
+    @DisplayName("Check amount of candidates to close the vacancy equal to limit")
+    void testValidateCandidateCountForClosureCandidatesEqualToLimit() {
+        vacancy.setCount(3);
+        vacancy.setCandidates(List.of(new Candidate(), new Candidate(), new Candidate()));
+
+        assertDoesNotThrow(() -> vacancyValidator.validateCandidateCountForClosure(vacancy));
+    }
+
+    @Test
+    @DisplayName("Check amount of candidates to close the vacancy is more than limit")
+    void testValidateCandidateCountForClosureCandidatesMoreThanLimit() {
+        vacancy.setCount(2);
+        vacancy.setCandidates(List.of(new Candidate(), new Candidate(), new Candidate()));
+
+        assertDoesNotThrow(() -> vacancyValidator.validateCandidateCountForClosure(vacancy));
+    }
+
+    @Test
+    @DisplayName("Check amount of candidates to close the vacancy is less than limit")
+    void testValidateCandidateCountForClosureCandidatesLessThanLimit() {
+        vacancy.setCount(4);
+        vacancy.setCandidates(List.of(new Candidate(), new Candidate(), new Candidate()));
+
+        Exception ex = assertThrows(InsufficientCandidatesException.class,
+                () -> vacancyValidator.validateCandidateCountForClosure(vacancy));
+        assertEquals("Vacancy should have at least 4 candidates to be closed", ex.getMessage());
     }
 
     private VacancyDto createTestVacancyDto() {
@@ -104,6 +154,22 @@ class VacancyValidatorTest {
         return TeamMember.builder()
                 .id(1L)
                 .roles(List.of(TeamRole.OWNER))
+                .build();
+    }
+
+    private Vacancy createTestVacancy() {
+        return Vacancy.builder()
+                .id(1L)
+                .name("Vacancy 1")
+                .description("Vacancy 1 description")
+                .status(VacancyStatus.OPEN)
+                .candidates(List.of(new Candidate()))
+                .project(Project.builder().id(1L).build())
+                .createdAt(LocalDateTime.now())
+                .salary(100.0)
+                .workSchedule(WorkSchedule.FULL_TIME)
+                .count(1)
+                .requiredSkillIds(List.of(1L))
                 .build();
     }
 }
