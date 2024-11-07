d81f58983686cf368c96347c22d5a47571dc792d
Maxim Gorobets <gorobets.maks@bk.ru>
2024-11-07 13:50:53 +1000

    InternshipMapper added

---
+++ src/main/java/faang/school/projectservice/mapper/InternshipMapper.java
@@ -0,0 +1,32 @@
+package faang.school.projectservice.mapper;
+
+import faang.school.projectservice.dto.client.internship.InternshipCreationDto;
+import faang.school.projectservice.dto.client.internship.InternshipDto;
+import faang.school.projectservice.model.Internship;
+import faang.school.projectservice.model.TeamMember;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface InternshipMapper {
+
+    @Mapping(source = "creatorUserId", target = "createdBy")
+    @Mapping(source = "interns", target = "interns", ignore = true)
+    Internship toEntity(InternshipCreationDto creationDto);
+
+    @Mapping(source = "mentorId.userId", target = "mentorUserId")
+    @Mapping(source = "createdBy", target = "creatorUserId")
+    @Mapping(source = "interns", target = "interns", qualifiedByName = "internsToIds")
+    InternshipDto toDto(Internship internship);
+
+    @Named("internsToIds")
+    default List<Long> internsToIds(List<TeamMember> interns) {
+        return interns.stream()
+                .map(TeamMember::getUserId)
+                .toList();
+    }
+}
