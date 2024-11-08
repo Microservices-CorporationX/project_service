f7f51c09331123e6a31667d882eb39796101b9c3
daurenberdibekov <berder08@gmail.com>
2024-11-08 12:57:27 +0100

    Add vacancy service, validator and exceptions

---
+++ src/main/java/faang/school/projectservice/exception/vacancy/VacancyDuplicationException.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.exception.vacancy;
+
+public class VacancyDuplicationException extends RuntimeException {
+    public VacancyDuplicationException(String message) {
+        super(message);
+    }
+}
