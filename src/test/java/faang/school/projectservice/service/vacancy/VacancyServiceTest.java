c4cab0285e49ae8d59232c63a13056e849e2b8df
Maks Russkikh <15maksim@mail.ru>
2024-11-09 13:19:16 +0300

    добавил тесты для фильтров. вынес получение данных из класса тестов

--- src/test/java/faang/school/projectservice/service/vacancy/VacancyServiceTest.java
+++ src/test/java/faang/school/projectservice/service/vacancy/VacancyServiceTest.java
@@ -1,4 +1,4 @@
-package faang.school.projectservice.vacancy;
+package faang.school.projectservice.service.vacancy;
 
 import faang.school.projectservice.config.context.UserContext;
 import faang.school.projectservice.dto.vacancy.VacancyDto;
@@ -12,13 +12,11 @@ import faang.school.projectservice.model.Candidate;
 import faang.school.projectservice.model.CandidateStatus;
 import faang.school.projectservice.model.Project;
 import faang.school.projectservice.model.TeamMember;
-import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.model.Vacancy;
 import faang.school.projectservice.model.VacancyStatus;
 import faang.school.projectservice.repository.CandidateRepository;
 import faang.school.projectservice.repository.ProjectRepository;
 import faang.school.projectservice.repository.VacancyRepository;
-import faang.school.projectservice.service.vacancy.VacancyService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,8 +25,6 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -37,6 +33,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -70,16 +68,16 @@ class VacancyServiceTest {
     @DisplayName("create vacancy positive")
     void createVacancyPositiveTest() {
         Long id = 1L;
-        VacancyDto actualVacancyDto = getVacancyDto();
+        VacancyDto actualVacancyDto = VacancyTestData.getVacancyDto();
         Project project = new Project();
         project.setOwnerId(id);
         Vacancy vacancy = vacancyMapper.vacancyDtoToVacancy(actualVacancyDto);
         vacancy.setProject(project);
         vacancy.setCreatedBy(id);
         vacancy.setStatus(VacancyStatus.OPEN);
-        List<Candidate> candidates = getCandidates();
+        List<Candidate> candidates = VacancyTestData.getCandidates();
         vacancy.setCandidates(candidates);
-        List<TeamMember> teamMembers = getTeamMembers();
+        List<TeamMember> teamMembers = VacancyTestData.getTeamMembers();
 
         when(userContext.getUserId()).thenReturn(id);
         when(teamMemberJpaRepository.findByUserId(id)).thenReturn(teamMembers);
@@ -97,8 +95,8 @@ class VacancyServiceTest {
     @DisplayName("test update method")
     void testPositiveUpdateMethod() {
         Long id = 1L;
-        VacancyDto actualVacancyDto = getVacancyDto();
-        Vacancy vacancy = getVacancy(1L, "Alex");
+        VacancyDto actualVacancyDto = VacancyTestData.getVacancyDto();
+        Vacancy vacancy = VacancyTestData.getVacancy(1L, "Alex");
         vacancy.setUpdatedBy(id);
         vacancy.setCount(0);
 
@@ -114,12 +112,12 @@ class VacancyServiceTest {
     @Test
     @DisplayName("test negative close method")
     void testNegativeCloseMethod() {
-        VacancyDto actualVacancyDto = getVacancyDto();
+        VacancyDto actualVacancyDto = VacancyTestData.getVacancyDto();
         actualVacancyDto.setId(1L);
-        Vacancy vacancy = getVacancy(1L, "Alex");
+        Vacancy vacancy = VacancyTestData.getVacancy(1L, "Alex");
         vacancy.setCount(1);
         vacancy.setStatus(VacancyStatus.CLOSED);
-        List<Candidate> candidates = getCandidates();
+        List<Candidate> candidates = VacancyTestData.getCandidates();
         candidates.forEach(candidate -> candidate.setCandidateStatus(CandidateStatus.ACCEPTED));
 
         when(vacancyRepository.findById(any())).thenReturn(Optional.of(vacancy));
@@ -134,12 +132,12 @@ class VacancyServiceTest {
     @Test
     @DisplayName("test positive close method")
     void testPositiveCloseMethod() {
-        VacancyDto actualVacancyDto = getVacancyDto();
+        VacancyDto actualVacancyDto = VacancyTestData.getVacancyDto();
         actualVacancyDto.setId(1L);
-        Vacancy vacancy = getVacancy(1L, "Alex");
+        Vacancy vacancy = VacancyTestData.getVacancy(1L, "Alex");
         vacancy.setCount(1);
         vacancy.setStatus(VacancyStatus.CLOSED);
-        List<Candidate> candidates = getCandidates();
+        List<Candidate> candidates = VacancyTestData.getCandidates();
         candidates.forEach(candidate -> {
             candidate.setCandidateStatus(CandidateStatus.ACCEPTED);
             candidate.setVacancy(vacancy);
@@ -148,6 +146,7 @@ class VacancyServiceTest {
 
         when(vacancyRepository.findById(any())).thenReturn(Optional.of(vacancy));
         when(vacancyMapper.vacancyToVacancyDto(any())).thenReturn(actualVacancyDto);
+        verify(vacancyRepository, never()).save(vacancy);
 
         VacancyDto inspectedDto = vacancyService.closeVacancy(actualVacancyDto);
         assertEquals(inspectedDto.getName(), actualVacancyDto.getName());
@@ -160,10 +159,10 @@ class VacancyServiceTest {
         VacancyFilter filterById = mock(VacancyFilterById.class);
         VacancyFilter filterByName = mock(VacancyFilterByName.class);
 
-        List<Vacancy> actualVacancies = List.of(getVacancy(1L, "Alex"));
-        List<Vacancy> vacancies = getVacanciesStream().toList();
-        VacancyFilterDto vacancyFilterDto = getFilterDto();
-        VacancyDto vacancyDto = getVacancyDto();
+        List<Vacancy> actualVacancies = List.of(VacancyTestData.getVacancy(1L, "Alex"));
+        List<Vacancy> vacancies = VacancyTestData.getVacanciesStream().toList();
+        VacancyFilterDto vacancyFilterDto = VacancyTestData.getFilterDto();
+        VacancyDto vacancyDto = VacancyTestData.getVacancyDto();
 
         when(filterById.isApplicable(any())).thenReturn(true);
         when(filterById.apply(any(), any())).thenReturn(actualVacancies.stream());
@@ -181,63 +180,5 @@ class VacancyServiceTest {
         assertEquals(actualVacanciesDto.get(0), inspectVacanciesDto.get(0));
         assertTrue(inspectVacanciesDto.contains(vacancyDto));
         assertTrue(actualVacanciesDto.size() == 1);
-    }
-
-    private Stream<Vacancy> getVacanciesStream() {
-        Vacancy vacancy = Vacancy.builder()
-                .id(1L)
-                .name("Alex").build();
-        Vacancy vacancy1 = Vacancy.builder()
-                .id(2L)
-                .name("Evgen").build();
-        Vacancy vacancy2 = Vacancy.builder()
-                .id(3L)
-                .name("Alex").build();
-        Vacancy vacancy3 = Vacancy.builder()
-                .id(4L)
-                .name("Olga").build();
-        return Stream.of(vacancy, vacancy1, vacancy2, vacancy3);
-    }
-
-    private List<Candidate> getCandidates() {
-        Candidate candidate = new Candidate();
-        candidate.setId(1L);
-        Candidate candidate1 = new Candidate();
-        candidate1.setId(2L);
-        Candidate candidate2 = new Candidate();
-        candidate1.setId(2L);
-        return Arrays.asList(candidate, candidate1, candidate2);
-    }
-
-    private VacancyFilterDto getFilterDto() {
-        return VacancyFilterDto.builder()
-                .id(1L)
-                .namePattern("Alex").build();
-    }
-
-    private VacancyDto getVacancyDto() {
-        return VacancyDto.builder()
-                .id(1L)
-                .name("Alex")
-                .projectId(1L)
-                .candidateIds(List.of(1L, 2L))
-                .build();
-    }
-
-    private Vacancy getVacancy(Long id, String name) {
-        return Vacancy.builder()
-                .id(id)
-                .name(name)
-                .candidates(new ArrayList<>())
-                .build();
-    }
-
-    private List<TeamMember> getTeamMembers() {
-        TeamMember teamMember = new TeamMember();
-        teamMember.setId(1L);
-        teamMember.setRoles(new ArrayList<>());
-        teamMember.getRoles().add(TeamRole.OWNER);
-        teamMember.getRoles().add(TeamRole.DESIGNER);
-        return List.of(teamMember);
     }
 }
