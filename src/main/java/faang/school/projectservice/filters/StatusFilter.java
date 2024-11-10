1ad4c3995b3882940c2715fcb96a2fd1e56fa515
SerUKalinin <serukalinin@gmail.com>
2024-11-10 21:34:17 +0300

    Добавил StatusFilter

---
+++ src/main/java/faang/school/projectservice/filters/StatusFilter.java
@@ -0,0 +1,15 @@
+package faang.school.projectservice.filters;
+
+import faang.school.projectservice.model.stage_invitation.StageInvitation;
+import faang.school.projectservice.model.stage_invitation.StageInvitationStatus;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StatusFilter implements InvitationFilter {
+    private final StageInvitationStatus status;
+
+    @Override
+    public boolean apply(StageInvitation invitation) {
+        return invitation.getStatus().equals(status);
+    }
+}
