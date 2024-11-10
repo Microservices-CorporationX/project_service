859fa98c21c1d773553de2fb4b1e7de1ac346eca
SerUKalinin <serukalinin@gmail.com>
2024-11-10 14:03:02 +0300

    import org.mapstruct.Mapping;  в interface StageInvitationMapper

--- src/main/java/faang/school/projectservice/mappers/invitation/StageInvitationMapper.java
+++ src/main/java/faang/school/projectservice/mappers/invitation/StageInvitationMapper.java
@@ -3,9 +3,18 @@ package faang.school.projectservice.mappers.invitation;
 import faang.school.projectservice.dto.invitation.StageInvitationDTO;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface StageInvitationMapper {
+
+    @Mapping(source = "stage.id", target = "stageId")
+    @Mapping(source = "author.id", target = "authorId")
+    @Mapping(source = "invited.id", target = "inviteeId")
     StageInvitationDTO toDto(StageInvitation stageInvitation);
+
+    @Mapping(source = "stageId", target = "stage.id")
+    @Mapping(source = "authorId", target = "author.id")
+    @Mapping(source = "inviteeId", target = "invited.id")
     StageInvitation toEntity(StageInvitationDTO stageInvitationDTO);
 }
