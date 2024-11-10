0b02b04c2a5b8e02c65e530a67e6839176ac1573
SerUKalinin <serukalinin@gmail.com>
2024-11-10 21:35:34 +0300

    Коммит после кросс реввью

--- src/main/java/faang/school/projectservice/dto/invitation/StageInvitationDTO.java
+++ src/main/java/faang/school/projectservice/dto/invitation/StageInvitationDTO.java
@@ -13,20 +13,14 @@ import lombok.NoArgsConstructor;
 public class StageInvitationDTO {
 
     private Long id;
-
     @NotNull(message = "Идентификатор этапа не может быть null")
     private Long stageId;
-
     @NotNull(message = "Идентификатор автора не может быть null")
     private Long authorId;
-
     @NotNull(message = "Идентификатор приглашенного не может быть null")
     private Long inviteeId;
-
-
     @NotNull(message = "Статус не может быть null")
     private StageInvitationStatus status;
-
     @Size(max = 255, message = "Причина отклонения слишком длинная")
     private String rejectionReason;
 }
