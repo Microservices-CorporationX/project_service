90f212c06bddc7300d2a879b1bffe4ebefa51180
SerUKalinin <serukalinin@gmail.com>
2024-11-10 21:30:21 +0300

    Добавил RejectionReasonDTO

---
+++ src/main/java/faang/school/projectservice/dto/invitation/RejectionReasonDTO.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice.dto.invitation;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class RejectionReasonDTO {
+
+    @NotBlank(message = "Причина отклонения не может быть пустой")
+    private String reason;
+}
