baefba362fc61858d393a59665b9a9c9a2b8d196
VovaBuzivskyi <vov4ikvseti@gmail.com>
2024-11-11 06:34:02 +0700

    refactor: change Mapping

--- src/main/java/faang/school/projectservice/controller/stage_invitation/StageInvitationController.java
+++ src/main/java/faang/school/projectservice/controller/stage_invitation/StageInvitationController.java
@@ -5,10 +5,10 @@ import faang.school.projectservice.dto.stage_invitation.StageInvitationFilterDto
 import faang.school.projectservice.service.stage_invitation.StageInvitationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +21,7 @@ public class StageInvitationController {
     private final StageInvitationService stageInvitationService;
 
     @PostMapping("/send/{invitorId}")
-    public void sendStageInvitation(@PathVariable long invitorId, @RequestParam @Valid StageInvitationDto dto) {
+    public void sendStageInvitation(@PathVariable long invitorId, @RequestBody @Valid StageInvitationDto dto) {
         stageInvitationService.sendStageInvitation(invitorId, dto);
     }
 
@@ -32,13 +32,13 @@ public class StageInvitationController {
 
     @PutMapping("/reject/{teamMemberId}")
     public void rejectStageInvitation(@PathVariable long teamMemberId, @RequestParam long stageInvitationId,
-                                      @RequestParam String rejectReason) {
+                                      @RequestBody String rejectReason) {
         stageInvitationService.rejectStageInvitation(teamMemberId, stageInvitationId, rejectReason);
     }
 
-    @GetMapping("/{teamMemberId}")
+    @PostMapping("/{teamMemberId}")
     public List<StageInvitationDto> getStageInvitations(
-            @PathVariable long teamMemberId, @RequestParam StageInvitationFilterDto filter) {
+            @PathVariable long teamMemberId, @RequestBody StageInvitationFilterDto filter) {
         return stageInvitationService.getStageInvitations(teamMemberId, filter);
     }
 }
