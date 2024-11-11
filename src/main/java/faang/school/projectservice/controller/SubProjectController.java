cd2bfa7cd5525e17d0b44de4d0cb20e6ed239f87
Alexander Litovkin <litovkinalexan@gmail.com>
2024-11-11 15:09:50 +0700

    implement sub project controller

---
+++ src/main/java/faang/school/projectservice/controller/SubProjectController.java
@@ -0,0 +1,55 @@
+package faang.school.projectservice.controller;
+
+import faang.school.projectservice.dto.project.ProjectDto;
+import faang.school.projectservice.dto.subproject.CreateSubProjectDto;
+import faang.school.projectservice.dto.subproject.SubProjectFilterDto;
+import faang.school.projectservice.service.ProjectService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/project/{projectId}/sub-project")
+@RequiredArgsConstructor
+public class SubProjectController {
+
+    private final ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectDto>> findSubProjects(@PathVariable Long projectId,
+                                                            SubProjectFilterDto filters,
+                                                            @RequestHeader("x-user-id") Long userId) {
+        return ResponseEntity.ok(projectService.findSubProjects(projectId, filters, userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createSubProject(@PathVariable("projectId") Long projectId,
+                                                 @Valid @RequestBody CreateSubProjectDto dto) {
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(projectService.createSubProject(projectId, dto).getId())
+                        .toUri()
+        ).build();
+    }
+
+    @PutMapping("/{subProjectId}")
+    public ResponseEntity<Void> updateSubProject(@PathVariable Long projectId,
+                                                 @PathVariable Long subProjectId,
+                                                 @Valid @RequestBody ProjectDto dto,
+                                                 @RequestHeader("x-user-id") Long userId) {
+        projectService.updateSubProject(projectId, subProjectId, dto, userId);
+         return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
@@ -2,6 +2,9 @@ package faang.school.projectservice.dto.project;
 
 import faang.school.projectservice.model.ProjectStatus;
 import faang.school.projectservice.model.ProjectVisibility;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +17,24 @@ import lombok.NoArgsConstructor;
 public class ProjectDto {
 
     private Long id;
+
+    @NotNull
     private Long ownerId;
+
+    @NotNull
     private Long parentId;
+
+    @NotBlank
+    @Size(max = 128)
     private String name;
+
+    @NotBlank
+    @Size(max = 4096)
     private String description;
+
+    @NotNull
     private ProjectStatus status;
+
+    @NotNull
     private ProjectVisibility visibility;
 }
