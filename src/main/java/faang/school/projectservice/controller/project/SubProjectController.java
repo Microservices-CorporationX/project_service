04c720519b8e8abdcb493a0f6f1c95634b856ce6
AlekseyB86nb <tigrxam@gmail.com>
2024-11-12 11:58:11 +0300

    поправил @NonNull на @NotNull в контроллере

--- src/main/java/faang/school/projectservice/controller/project/SubProjectController.java
+++ src/main/java/faang/school/projectservice/controller/project/SubProjectController.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +26,7 @@ public class SubProjectController {
 
     @PostMapping("/project/{parentProjectId}")
     @Operation(summary = "Create sub project in DB")
-    public CreateSubProjectDto createSubProject(@PathVariable @Positive @NonNull Long parentProjectId,
+    public CreateSubProjectDto createSubProject(@PathVariable @Positive @NotNull Long parentProjectId,
                                                 @RequestBody @Valid CreateSubProjectDto subProjectDto) {
         return projectService.createSubProject(parentProjectId, subProjectDto);
     }
@@ -35,7 +35,7 @@ public class SubProjectController {
     @Operation(summary = "Update project by id",
             parameters = @Parameter(name = "x-user-id", in = ParameterIn.HEADER, required = true, description = "User id")
     )
-    public CreateSubProjectDto updateProject(@PathVariable @Positive @NonNull Long projectId,
+    public CreateSubProjectDto updateProject(@PathVariable @Positive @NotNull Long projectId,
                                              @RequestBody @Valid CreateSubProjectDto subProjectDto) {
         long userId = userContext.getUserId();
         return projectService.updateProject(projectId, subProjectDto, userId);
@@ -43,7 +43,7 @@ public class SubProjectController {
 
     @GetMapping("/project/{projectId}/projects")
     @Operation(summary = "Get sub projects by parent project id and filters")
-    public List<CreateSubProjectDto> getProjectsByFilters(@PathVariable @Positive @NonNull Long projectId,
+    public List<CreateSubProjectDto> getProjectsByFilters(@PathVariable @Positive @NotNull Long projectId,
                                                           @RequestBody @Valid FilterProjectDto filterDto
     ) {
         return projectService.getProjectsByFilter(projectId, filterDto);
