5ab549a9c3e715c7790672ea69450e836d14c178
SerUKalinin <serukalinin@gmail.com>
2024-11-13 20:46:27 +0300

    Коммит после кросс реввью v3.0

--- src/main/java/faang/school/projectservice/handler/invitation/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/handler/invitation/GlobalExceptionHandler.java
@@ -4,18 +4,20 @@ import faang.school.projectservice.exceptions.invitation.InvitationNotFoundExcep
 import faang.school.projectservice.exceptions.invitation.InvalidInvitationDataException;
 import faang.school.projectservice.exceptions.invitation.StageNotFoundException;
 import faang.school.projectservice.exceptions.invitation.TeamMemberNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Component(value = "globalExceptionHandler")
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
