ae4defdd59102adef21441b0d89e3e9dcd8a199d
SerUKalinin <serukalinin@gmail.com>
2024-11-17 19:19:31 +0300

    Commit

--- src/main/java/faang/school/projectservice/mapper/invitation/StageInvitationMapper.java
+++ src/main/java/faang/school/projectservice/mapper/invitation/StageInvitationMapper.java
@@ -2,7 +2,9 @@ package faang.school.projectservice.mapper.invitation;
 
 import faang.school.projectservice.dto.invitation.StageInvitationDTO;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface StageInvitationMapper {
 
     StageInvitation toEntity(StageInvitationDTO stageInvitationDTO);
