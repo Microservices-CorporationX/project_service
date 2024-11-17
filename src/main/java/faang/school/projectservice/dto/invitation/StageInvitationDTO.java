f41d07cf39c8e29ac53c00d24c4f454dd309c927
SerUKalinin <serukalinin@gmail.com>
2024-11-17 13:09:10 +0300

    Commit

--- src/main/java/faang/school/projectservice/dto/invitation/StageInvitationDTO.java
+++ src/main/java/faang/school/projectservice/dto/invitation/StageInvitationDTO.java
@@ -2,14 +2,25 @@ package faang.school.projectservice.dto.invitation;
 
 import faang.school.projectservice.model.stage_invitation.StageInvitationStatus;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class StageInvitationDTO {
 
     private Long id;
-    @NotNull(message = "Description is required")
     private String description;
     private StageInvitationStatus status;
+    @NotNull(message = "Идентификатор этапа не может быть null")
     private Long stageId;
+    @NotNull(message = "Идентификатор автора не может быть null")
     private Long authorId;
+    @NotNull(message = "Идентификатор приглашенного не может быть null")
     private Long invitedId;
 }
--- src/main/java/faang/school/projectservice/service/invitation/StageInvitationService.java
+++ src/main/java/faang/school/projectservice/service/invitation/StageInvitationService.java
@@ -1,9 +1,13 @@
 package faang.school.projectservice.service.invitation;
 
+import faang.school.projectservice.dto.invitation.StageInvitationDTO;
 import faang.school.projectservice.mapper.invitation.StageInvitationMapper;
+import faang.school.projectservice.model.TeamMember;
+import faang.school.projectservice.model.stage.Stage;
 import faang.school.projectservice.repository.StageInvitationRepository;
 import faang.school.projectservice.repository.StageRepository;
 import faang.school.projectservice.repository.TeamMemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,4 +21,27 @@ public class StageInvitationService {
     private final StageRepository stageRepository;
     private final TeamMemberRepository teamMemberRepository;
     private final StageInvitationMapper stageInvitationMapper;
+
+    public StageInvitationDTO sendInvitation(StageInvitationDTO stageInvitationDTO) {
+        log.info("Получен запрос на отправку приглашения: {}", stageInvitationDTO);
+
+        Stage stage = stageRepository.getById((stageInvitationDTO.getStageId()));
+
+        TeamMember invited = teamMemberRepository.findById((stageInvitationDTO.getInvitedId()));
+        if (invited == null) {
+            log.warn("Приглашаемый участник не найден: {}", stageInvitationDTO.getInvitedId());
+            throw new EntityNotFoundException("Приглашаемый участник не найден");
+        }
+
+        TeamMember author = teamMemberRepository.findById((stageInvitationDTO.getAuthorId()));
+        if (author == null) {
+            log.warn("Автор не найден: {}", stageInvitationDTO.getAuthorId());
+            throw new EntityNotFoundException("Автор не найден");
+        }
+
+        if (!stage.getProject().equals(invited.getTeam().getProject())) {
+            log.warn("Приглашение не может быть отправлено: участник не принадлежит проекту этапа");
+            throw new InvalidIn
+        }
+    }
 }
