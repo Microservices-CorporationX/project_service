d0cfc1bfc4f33e6dee9ee5bfa259b7b2977805e1
SerUKalinin <serukalinin@gmail.com>
2024-11-17 12:29:44 +0300

    Сервис и Контройлер

--- src/main/java/faang/school/projectservice/service/invitation/StageInvitationService.java
+++ src/main/java/faang/school/projectservice/service/invitation/StageInvitationService.java
@@ -1,10 +1,20 @@
 package faang.school.projectservice.service.invitation;
 
+import faang.school.projectservice.mappers.invitation.StageInvitationMapperImpl;
+import faang.school.projectservice.repository.StageInvitationRepository;
+import faang.school.projectservice.repository.StageRepository;
+import faang.school.projectservice.repository.TeamMemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-
+@RequiredArgsConstructor
 public class StageInvitationService {
+
+    private final StageInvitationRepository stageInvitationRepository;
+    private final StageRepository stageRepository;
+    private final TeamMemberRepository teamMemberRepository;
+    private final StageInvitationMapperImpl
 }
