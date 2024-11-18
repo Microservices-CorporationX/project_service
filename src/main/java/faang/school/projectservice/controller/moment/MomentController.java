bf5f8c35f9dd89c786bc7ca4dcf795201b74e1e5
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-11-18 18:13:44 +0300

    Initial Commit

---
+++ src/main/java/faang/school/projectservice/controller/moment/MomentController.java
@@ -0,0 +1,4 @@
+package faang.school.projectservice.controller.moment;
+
+public class MomentController {
+}
---
+++ src/main/java/faang/school/projectservice/service/moment/MomentService.java
@@ -0,0 +1,4 @@
+package faang.school.projectservice.service.moment;
+
+public class MomentService {
+}
