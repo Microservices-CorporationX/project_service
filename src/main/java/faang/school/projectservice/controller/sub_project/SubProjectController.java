a98f1cf1dc3077fa812913f216253d3fbaa4b2ac
lomuserg <sergeymu2015@yandex.ru>
2024-11-21 22:36:29 +0300

    BJS2-40761: add SubProjectController swagger

--- src/main/java/faang/school/projectservice/controller/sub_project/SubProjectController.java
+++ src/main/java/faang/school/projectservice/controller/sub_project/SubProjectController.java
@@ -4,6 +4,12 @@ import faang.school.projectservice.dto.CreateSubProjectDto;
 import faang.school.projectservice.dto.ProjectDto;
 import faang.school.projectservice.dto.ProjectFilterDto;
 import faang.school.projectservice.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -20,20 +26,35 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/Projects")
+@Tag(name = "Контроллер для управления подпроектами")
 public class SubProjectController {
     private final ProjectService projectService;
 
     @PostMapping("/{Id}/createSubProject")
+    @Operation(summary = "Создать подпроект")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Project created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProjectDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Non-validate parameters",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Project already exists",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     public ProjectDto createSubProject(@NotNull @Min(0) @PathVariable("Id") Long projectId, @Valid @RequestBody CreateSubProjectDto createSubProjectDto) {
         return projectService.createSubProject(projectId, createSubProjectDto);
     }
 
     @PatchMapping("/{Id}/updateSubProject")
+
     public ProjectDto updateSubProject(@Valid @RequestBody ProjectDto subProjectDto) {
         return projectService.updateSubProject(subProjectDto);
     }
 
     @PostMapping("/{Id}/getSubProject")
+
     public List<ProjectDto> getSubProjects(@NotNull @Min(0) @PathVariable("Id") Long projectId, @RequestBody ProjectFilterDto projectFilterDto) {
         return projectService.getSubProjects(projectId, projectFilterDto);
     }
--- src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
@@ -6,10 +6,12 @@ import faang.school.projectservice.dto.VacancyFilterDto;
 import faang.school.projectservice.service.vacancy.VacancyService;
 import faang.school.projectservice.validator.vacancy.VacancyControllerValidator;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +26,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/project/vacancy")
+@Tag(name = "Контроллер для управления вакансиями")
 @RequiredArgsConstructor
 public class VacancyController {
 
@@ -34,7 +37,9 @@ public class VacancyController {
     @PostMapping
     @Operation(summary = "Создать вакансию")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Vacancy created"),
+            @ApiResponse(responseCode = "201", description = "Vacancy created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VacancyDto.class))}),
             @ApiResponse(responseCode = "400", description = "Non-validate parameters",
                     content = @Content),
             @ApiResponse(responseCode = "400", description = "Vacancy already exists",
@@ -50,7 +55,9 @@ public class VacancyController {
     @PutMapping
     @Operation(summary = "Обновить вакансию по id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Vacancy was updated"),
+            @ApiResponse(responseCode = "200", description = "Vacancy was updated",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = VacancyDto.class))}),
             @ApiResponse(responseCode = "400", description = "Non-validate parameters",
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "Vacancy not found",
@@ -74,7 +81,8 @@ public class VacancyController {
                     content = @Content),
             @ApiResponse(responseCode = "500", description = "Server error",
                     content = @Content)})
-    public void deleteVacancy(@PathVariable Long vacancyId) {
+    public void deleteVacancy(@Parameter(description = "id вакансии которую хотим удалить")
+                              @PathVariable Long vacancyId) {
         validator.validateVacancyId(vacancyId);
         vacancyService.deleteVacancy(vacancyId);
     }
@@ -106,7 +114,8 @@ public class VacancyController {
                     content = @Content),
             @ApiResponse(responseCode = "500", description = "Server error",
                     content = @Content)})
-    public VacancyDto getVacancy(@PathVariable Long vacancyId) {
+    public VacancyDto getVacancy(@Parameter(description = "id вакансии которую хотим получить")
+                                 @PathVariable Long vacancyId) {
         validator.validateVacancyId(vacancyId);
         return vacancyService.getVacancy(vacancyId);
     }
