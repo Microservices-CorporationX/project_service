815cbc947c18ba20ae51e673b20acd9129283a24
Naku <polevaya.nastena@internet.ru>
2024-11-23 19:06:12 +0300

    41090

--- src/main/java/faang/school/projectservice/controller/ResourceController.java
+++ src/main/java/faang/school/projectservice/controller/ResourceController.java
@@ -6,21 +6,17 @@ import faang.school.projectservice.service.ResourceService;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/project")
 public class ResourceController {
     private final ResourceService resourceService;
     private final UserContext userContext;
