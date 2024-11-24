ad1dc741ae17d8e2659ece4a44f195b0cb2f1aae
AlekseyB86 <avbelov86@mail.ru>
2024-11-24 14:20:21 +0300

    тест на удаление ресурса

--- src/test/java/faang/school/projectservice/service/resource/ResourceServiceTest.java
+++ src/test/java/faang/school/projectservice/service/resource/ResourceServiceTest.java
@@ -1,5 +1,6 @@
 package faang.school.projectservice.service.resource;
 
+import faang.school.projectservice.dto.resource.RequestDeleteResourceDto;
 import faang.school.projectservice.dto.resource.ResponseResourceDto;
 import faang.school.projectservice.jpa.ResourceRepository;
 import faang.school.projectservice.mapper.resource.ResourceMapper;
@@ -21,8 +22,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -33,7 +36,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class ResourceServiceTest {
     @InjectMocks
-    private  ResourceService resourceService;
+    private ResourceService resourceService;
 
     @Mock
     private ProjectService projectService;
@@ -88,6 +91,7 @@ class ResourceServiceTest {
         when(multipartFile.getContentType()).thenReturn("image/png");
         when(multipartFile.getSize()).thenReturn(1_000L);
         when(resourceRepository.saveAll(anyList())).thenReturn(List.of(new Resource()));
+        doNothing().when(projectService).saveProject(project);
         when(resourceMapper.toDtoFromEntity(any())).thenReturn(responseResourceDto);
 
         List<ResponseResourceDto> result = resourceService.uploadResources(List.of(multipartFile), 1L, 1L);
@@ -100,15 +104,79 @@ class ResourceServiceTest {
     }
 
     @Test
-    void deleteResource() {
+    void deleteResource_Positive() {
+        RequestDeleteResourceDto requestDto = RequestDeleteResourceDto.builder()
+                .id(1L)
+                .build();
+        TeamMember teamMember = TeamMember.builder()
+                .id(1L)
+                .roles(new ArrayList<>(List.of(TeamRole.MANAGER)))
+                .build();
+        Project project = Project.builder()
+                .id(4L)
+                .name("project-4")
+                .storageSize(BigInteger.valueOf(1_001_000))
+                .maxStorageSize(BigInteger.valueOf(2_000_000))
+                .resources(new ArrayList<>())
+                .build();
+        Resource resource = createResource(project, teamMember);
+        Resource resourceDeleted = createResource(project, teamMember);
+        resourceDeleted.setStatus(ResourceStatus.DELETED);
+        resourceDeleted.setKey(null);
+        resourceDeleted.setSize(null);
+        resourceDeleted.setUpdatedBy(teamMember);
+        resourceDeleted.setUpdatedAt(LocalDateTime.now());
+
+        when(resourceRepository.findById(anyLong())).thenReturn(Optional.ofNullable(resource));
+        doNothing().when(resourceValidator).checkUserInProject(anyLong(), any(), any());
+        doNothing().when(s3Util).s3DeleteFile(anyString());
+        doNothing().when(projectService).saveProject(project);
+        when(resourceRepository.save(any())).thenReturn(resourceDeleted);
+
+        resourceService.deleteResource(1L, requestDto);
+
+        verify(resourceRepository).findById(anyLong());
+        verify(resourceValidator).checkUserInProject(anyLong(), any(), any());
+        verify(s3Util).s3DeleteFile(anyString());
+        verify(resourceRepository).save(any());
+        verify(projectService).saveProject(any());
+
+        assertEquals(ResourceStatus.DELETED, resource.getStatus());
     }
 
     @Test
     void getResource() {
+        Resource resource = Resource.builder()
+                .id(1L)
+                .name("TestName")
+                .key("4project-4/TestName")
+                .size(BigInteger.valueOf(1_000))
+                .allowedRoles(new ArrayList<>(List.of(TeamRole.MANAGER)))
+                .type(ResourceType.IMAGE)
+                .status(ResourceStatus.ACTIVE)
+                .build();
+        when(resourceRepository.findById(1L)).thenReturn(Optional.of(resource));
+        Resource result = resourceService.getResource(1L);
+        assertEquals(resource, result);
     }
 
     @Test
     void updateResource() {
+    }
+
+    private static Resource createResource(Project project, TeamMember teamMember) {
+        return Resource.builder()
+                .id(1L)
+                .name("TestName")
+                .key("4project-4/TestName")
+                .size(BigInteger.valueOf(1_000))
+                .allowedRoles(new ArrayList<>(List.of(TeamRole.MANAGER)))
+                .type(ResourceType.IMAGE)
+                .status(ResourceStatus.ACTIVE)
+                .project(project)
+                .createdBy(teamMember)
+                .updatedBy(teamMember)
+                .build();
     }
 
     //        Project updateProject = Project.builder()
