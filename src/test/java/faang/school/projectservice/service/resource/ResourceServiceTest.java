17f2cfe26e99100e6417291d31d55cf933bff9b6
AlekseyB86 <avbelov86@mail.ru>
2024-11-24 16:48:40 +0300

    тест на обновление ресурса

--- src/test/java/faang/school/projectservice/service/resource/ResourceServiceTest.java
+++ src/test/java/faang/school/projectservice/service/resource/ResourceServiceTest.java
@@ -55,29 +55,12 @@ class ResourceServiceTest {
 
     @Test
     void testUploadResources_Positive() {
-        TeamMember teamMember = TeamMember.builder()
-                .id(1L)
-                .roles(new ArrayList<>(List.of(TeamRole.MANAGER)))
-                .build();
-        Project project = Project.builder()
-                .id(4L)
-                .name("project-4")
-                .storageSize(BigInteger.valueOf(1_000_000))
-                .maxStorageSize(BigInteger.valueOf(2_000_000))
-                .resources(new ArrayList<>())
-                .build();
-        Resource resource = Resource.builder()
-                .id(1L)
-                .name("TestName")
-                .key("4project-4/TestName")
-                .size(BigInteger.valueOf(1_000))
-                .allowedRoles(new ArrayList<>(List.of(TeamRole.MANAGER)))
-                .type(ResourceType.IMAGE)
-                .status(ResourceStatus.ACTIVE)
-                .createdBy(teamMember)
-                .updatedBy(teamMember)
-                .project(project)
-                .build();
+        TeamMember teamMember = createTeamMember();
+        Project project = createProject();
+        Resource resourceSaved = createResource(project, teamMember);
+        Project updateProject = createProject();
+        updateProject.setStorageSize(BigInteger.valueOf(1_001_000));
+        updateProject.getResources().add(resourceSaved);
         ResponseResourceDto responseResourceDto = ResponseResourceDto.builder()
                 .id(1L)
                 .key("4project-4/TestName")
@@ -90,12 +73,11 @@ class ResourceServiceTest {
         when(multipartFile.getOriginalFilename()).thenReturn("TestName");
         when(multipartFile.getContentType()).thenReturn("image/png");
         when(multipartFile.getSize()).thenReturn(1_000L);
-        when(resourceRepository.saveAll(anyList())).thenReturn(List.of(new Resource()));
-        doNothing().when(projectService).saveProject(project);
+        when(resourceRepository.saveAll(anyList())).thenReturn(List.of(resourceSaved));
         when(resourceMapper.toDtoFromEntity(any())).thenReturn(responseResourceDto);
 
         List<ResponseResourceDto> result = resourceService.uploadResources(List.of(multipartFile), 1L, 1L);
-        verify(s3Util).s3UploadFile(multipartFile, resource.getKey());
+        verify(s3Util).s3UploadFile(multipartFile, resourceSaved.getKey());
         verify(resourceRepository).saveAll(anyList());
         verify(projectService).saveProject(any());
 
@@ -104,21 +86,14 @@ class ResourceServiceTest {
     }
 
     @Test
-    void deleteResource_Positive() {
+    void testDeleteResource_Positive() {
         RequestDeleteResourceDto requestDto = RequestDeleteResourceDto.builder()
                 .id(1L)
                 .build();
-        TeamMember teamMember = TeamMember.builder()
-                .id(1L)
-                .roles(new ArrayList<>(List.of(TeamRole.MANAGER)))
-                .build();
-        Project project = Project.builder()
-                .id(4L)
-                .name("project-4")
-                .storageSize(BigInteger.valueOf(1_001_000))
-                .maxStorageSize(BigInteger.valueOf(2_000_000))
-                .resources(new ArrayList<>())
-                .build();
+        TeamMember teamMember = createTeamMember();
+        Project project = createProject();
+        project.setStorageSize(BigInteger.valueOf(1_001_000));
+        project.getResources().add(createResource(project, teamMember));
         Resource resource = createResource(project, teamMember);
         Resource resourceDeleted = createResource(project, teamMember);
         resourceDeleted.setStatus(ResourceStatus.DELETED);
@@ -140,12 +115,10 @@ class ResourceServiceTest {
         verify(s3Util).s3DeleteFile(anyString());
         verify(resourceRepository).save(any());
         verify(projectService).saveProject(any());
-
-        assertEquals(ResourceStatus.DELETED, resource.getStatus());
     }
 
     @Test
-    void getResource() {
+    void testGetResource_Positive() {
         Resource resource = Resource.builder()
                 .id(1L)
                 .name("TestName")
@@ -161,7 +134,56 @@ class ResourceServiceTest {
     }
 
     @Test
-    void updateResource() {
+    void testUpdateResource_Positive() {
+        Project project = createProject();
+        TeamMember teamMember = createTeamMember();
+        Resource resource = createResource(project, teamMember);
+        project.setStorageSize(BigInteger.valueOf(1_002_000));
+        resource.setKey("4project-4/TestName");
+        resource.setSize(BigInteger.valueOf(2_000));
+        resource.setUpdatedBy(teamMember);
+        ResponseResourceDto responseResourceDto = ResponseResourceDto.builder()
+                .id(1L)
+                .build();
+
+        when(resourceRepository.findById(anyLong())).thenReturn(Optional.of(resource));
+        when(teamMemberService.getTeamMemberByUserIdAndProjectId(anyLong(), anyLong())).thenReturn(teamMember);
+        doNothing().when(resourceValidator).checkProjectStorageSizeExceeded(any(), any());
+        doNothing().when(s3Util).s3DeleteFile(anyString());
+        doNothing().when(s3Util).s3UploadFile(any(), anyString());
+        doNothing().when(projectService).saveProject(any());
+        when(resourceRepository.save(any())).thenReturn(resource);
+        when(resourceMapper.toDtoFromEntity(any())).thenReturn(responseResourceDto);
+
+        ResponseResourceDto result = resourceService.updateResource(multipartFile, 1L, 1L);
+        verify(resourceRepository).findById(anyLong());
+        verify(teamMemberService).getTeamMemberByUserIdAndProjectId(anyLong(), anyLong());
+        verify(resourceValidator).checkProjectStorageSizeExceeded(any(), any());
+        verify(s3Util).s3DeleteFile(anyString());
+        verify(s3Util).s3UploadFile(any(), anyString());
+        verify(projectService).saveProject(any());
+        verify(resourceRepository).save(any());
+        verify(resourceMapper).toDtoFromEntity(any());
+
+        assertEquals(responseResourceDto, result);
+    }
+
+    private static TeamMember createTeamMember() {
+        return TeamMember.builder()
+                .id(1L)
+                .roles(new ArrayList<>(List.of(TeamRole.MANAGER)))
+                .build();
+    }
+
+    private static Project createProject() {
+        return Project.builder()
+                .id(4L)
+                .name("project-4")
+                .storageSize(BigInteger.valueOf(1_000_000))
+                .maxStorageSize(BigInteger.valueOf(2_000_000))
+                .resources(new ArrayList<>())
+                .ownerId(1L)
+                .build();
     }
 
     private static Resource createResource(Project project, TeamMember teamMember) {
@@ -178,11 +200,4 @@ class ResourceServiceTest {
                 .updatedBy(teamMember)
                 .build();
     }
-
-    //        Project updateProject = Project.builder()
-//                .id(4L)
-//                .name("project-4")
-//                .storageSize(BigInteger.valueOf(1_001_000))
-//                .maxStorageSize(BigInteger.valueOf(2_000_000))
-//                .build();
 }
