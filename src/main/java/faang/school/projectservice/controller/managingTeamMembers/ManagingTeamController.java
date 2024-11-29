c666f19efd31b5372b50b2008110ddfb066f7921
SiarheiHaurylchuk <sggavrilchik@gmail.com>
2024-11-29 18:03:29 +0300

    finishTest

--- src/main/java/faang/school/projectservice/controller/managingTeamMembers/ManagingTeamController.java
+++ src/main/java/faang/school/projectservice/controller/managingTeamMembers/ManagingTeamController.java
@@ -5,6 +5,7 @@ import faang.school.projectservice.dto.teammember.TeamMemberDto;
 import faang.school.projectservice.service.managingTeamService.managingTeamService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,35 +17,47 @@ public class ManagingTeamController {
 
     private final managingTeamService ManagingTeamService;
 
-    @PostMapping()
-    public TeamMemberDto addTeamMember(@PathVariable Long projectId, @Valid @RequestBody TeamMemberDto TeamMemberDto, @PathVariable Long teamMemberId) {
-        return ManagingTeamService.addTeamMember(projectId, TeamMemberDto, teamMemberId);
+    @PostMapping("/{projectId}")
+    public ResponseEntity<TeamMemberDto> addTeamMember(
+            @PathVariable Long projectId,
+            @Valid @RequestBody TeamMemberDto teamMemberDto,
+            @RequestParam Long teamMemberId) {
+        return ResponseEntity.ok(ManagingTeamService.addTeamMember(projectId, teamMemberDto, teamMemberId));
     }
 
-    @PutMapping()
-    public TeamMemberDto updateTeamMember(@PathVariable Long projectId, @Valid @RequestBody TeamMemberDto TeamMemberDto, @PathVariable Long teamMemberId, @PathVariable Long currentUserId) {
-        return ManagingTeamService.updateTeamMember(projectId, TeamMemberDto, teamMemberId, currentUserId);
+    @PutMapping("/{projectId}")
+    public ResponseEntity<TeamMemberDto> updateTeamMember(
+            @PathVariable Long projectId,
+            @Valid @RequestBody TeamMemberDto teamMemberDto,
+            @RequestParam Long teamMemberId,
+            @RequestParam Long currentUserId) {
+        return ResponseEntity.ok(ManagingTeamService.updateTeamMember(projectId, teamMemberDto, teamMemberId, currentUserId));
     }
 
-    @DeleteMapping("/{teamMemberId}")
-    public TeamMemberDto deleteTeamMember(@PathVariable Long projectId, @PathVariable Long teamMemberId,  Long currentUserId) {
-        return ManagingTeamService.deleteTeamMember(projectId, teamMemberId, currentUserId);
+    @DeleteMapping("/{projectId}/{teamMemberId}")
+    public ResponseEntity<TeamMemberDto> deleteTeamMember(
+            @PathVariable Long projectId,
+            @PathVariable Long teamMemberId,
+            @RequestParam Long currentUserId) {
+        return ResponseEntity.ok(ManagingTeamService.deleteTeamMember(projectId, teamMemberId, currentUserId));
     }
 
-    @GetMapping()
-    public List<TeamMemberDto> getTeamMembers(@PathVariable Long projectId, @Valid @RequestBody TeamMemberFilterDto teamMemberFilterDto) {
-        return ManagingTeamService.getTeamMemberWithFilter(projectId, teamMemberFilterDto);
+    @GetMapping("/{projectId}")
+    public ResponseEntity<List<TeamMemberDto>> getTeamMembersWithFilter(
+            @PathVariable Long projectId,
+            @Valid @RequestBody TeamMemberFilterDto teamMemberFilterDto) {
+        return ResponseEntity.ok(ManagingTeamService.getTeamMemberWithFilter(projectId, teamMemberFilterDto));
     }
 
-
-    @GetMapping("/all")
-    public List<TeamMemberDto> getAllTeamMembers(Long projectId) {
-        return ManagingTeamService.getAllMembers(projectId);
+    @GetMapping("/{projectId}/all")
+    public ResponseEntity<List<TeamMemberDto>> getAllTeamMembers(@PathVariable Long projectId) {
+        return ResponseEntity.ok(ManagingTeamService.getAllMembers(projectId));
     }
 
-    @GetMapping("/{teamMemberId}")
-    public TeamMemberDto getTeamMember(@PathVariable Long projectId, @PathVariable Long teamMemberId) {
-        return ManagingTeamService.getTeamMember(projectId, teamMemberId);
+    @GetMapping("/{projectId}/{teamMemberId}")
+    public ResponseEntity<TeamMemberDto> getTeamMember(
+            @PathVariable Long projectId,
+            @PathVariable Long teamMemberId) {
+        return ResponseEntity.ok(ManagingTeamService.getTeamMember(projectId, teamMemberId));
     }
-
 }
--- src/test/java/faang/school/projectservice/controller/teamMemberController/teamMemberControllerTest.java
+++ src/test/java/faang/school/projectservice/controller/teamMemberController/teamMemberControllerTest.java
@@ -1,11 +1,351 @@
 package faang.school.projectservice.controller.teamMemberController;
-import faang.school.projectservice.controller.managingTeamMembers.ManagingTeamController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.projectservice.controller.managingTeamMembers.ManagingTeamController;
+import faang.school.projectservice.dto.managingTeamDto.TeamMemberFilterDto;
+import faang.school.projectservice.dto.teammember.TeamMemberDto;
+import faang.school.projectservice.service.managingTeamService.managingTeamService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ManagingTeamController.class)
-
+@ContextConfiguration(classes = {managingTeamService.class, ManagingTeamController.class})
 public class teamMemberControllerTest {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private managingTeamService ManagingTeamService;
+
+    @Test
+    public void testAddTeamMemberPositive() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.addTeamMember(1L, teamMemberDto, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(post("/managing-team/1", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(teamMemberDto)));
+    }
+
+    @Test
+    public void testAddTeamMemberNegative() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+        when(ManagingTeamService.addTeamMember(1L, teamMemberDto, 1L)).thenReturn(null);
+
+        mockMvc.perform(post("/managing-team/1", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testAddTeamMemberNegativeNotFound() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.addTeamMember(1L, teamMemberDto, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(post("/managing-team", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testPutTeamMemberPositive() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.updateTeamMember(1L, teamMemberDto, 1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/managing-team/{projectId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1")
+                        .param("currentUserId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(teamMemberDto)));
+    }
+
+    @Test
+    public void testPutTeamMemberNegative() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+        when(ManagingTeamService.updateTeamMember(1L, teamMemberDto, 1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/managing-team/{projectId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1")
+                        .param("currentUserId", "1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testPutTeamMemberNegativeIsNotFound() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+        when(ManagingTeamService.updateTeamMember(1L, teamMemberDto, 1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/managing-team", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1")
+                        .param("currentUserId", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteTeamMemberPositive() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.deleteTeamMember(1L, 1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/managing-team/{projectId}/{teamMemberId}", 1L, 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("currentUserId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(teamMemberDto)));
+    }
+
+    @Test
+    public void testDeleteTeamMemberNegative() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(2L);
+        teamMemberDto.setTeam(2L);
+        teamMemberDto.setUserId(2L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.deleteTeamMember(1L, 1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/managing-team/{projectId}/{teamMemberId}", 1L, 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testDeleteTeamMemberNegativeNotFound() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.deleteTeamMember(1L, 1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/managing-team/", 1L, 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("currentUserId", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testGetTeamMembersWithFilterPositive() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        TeamMemberFilterDto teamMemberFilterDto = new TeamMemberFilterDto();
+        teamMemberFilterDto.setRole("Developer");
+
+        when(ManagingTeamService.getTeamMemberWithFilter(1L, teamMemberFilterDto))
+                .thenReturn(List.of(teamMemberDto));
+
+        mockMvc.perform(get("/managing-team/{projectId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberFilterDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(List.of(teamMemberDto))));
+    }
+
+    @Test
+    public void testGetTeamMembersWithFilterNegative() throws Exception {
+        TeamMemberFilterDto teamMemberFilterDto = new TeamMemberFilterDto();
+
+        when(ManagingTeamService.getTeamMemberWithFilter(1L, teamMemberFilterDto))
+                .thenReturn(List.of());
+
+        mockMvc.perform(get("/managing-team/{projectId}", 1L)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberFilterDto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetTeamMembersWithFilterPositiveNotFound() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        TeamMemberFilterDto teamMemberFilterDto = new TeamMemberFilterDto();
+        teamMemberFilterDto.setRole("Developer");
+
+        when(ManagingTeamService.getTeamMemberWithFilter(1L, teamMemberFilterDto))
+                .thenReturn(List.of(teamMemberDto));
+
+        mockMvc.perform(get("/managing-team/", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberFilterDto)))
+                .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    public void testGetAllTeamMembersAllPositive() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.getAllMembers(1L)).thenReturn(List.of(teamMemberDto));
+
+        mockMvc.perform(get("/managing-team/{projectId}/all", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(List.of(teamMemberDto))));
+    }
+
+    @Test
+    public void testGetAllTeamMembersAllNegative() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+        when(ManagingTeamService.getAllMembers(1L)).thenReturn(null);
+
+        mockMvc.perform(get("/managing-team/all", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetAllTeamMembersAllNegativeNotFound() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+        when(ManagingTeamService.getAllMembers(1L)).thenReturn(null);
+
+        mockMvc.perform(get("/managing-team/", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(OBJECT_MAPPER.writeValueAsString(teamMemberDto))
+                        .param("teamMemberId", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testGetTeamMemberPositive() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+        teamMemberDto.setId(1L);
+        teamMemberDto.setTeam(1L);
+        teamMemberDto.setUserId(1L);
+        teamMemberDto.setUsername("testUser");
+        teamMemberDto.setRoles(List.of("Developer"));
+        teamMemberDto.setDescription("Team member description");
+        teamMemberDto.setAccessLevel(1);
+
+        when(ManagingTeamService.getTeamMember(1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(get("/managing-team/{projectId}/{teamMemberId}", 1L, 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("currentUserId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(teamMemberDto)));
+    }
+
+    @Test
+    public void testGetTeamMemberNegative() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+
+        when(ManagingTeamService.getTeamMember(1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(get("/managing-team/{projectId}/{teamMemberId}", 1L, 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("currentUserId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(OBJECT_MAPPER.writeValueAsString(teamMemberDto)));
+    }
+
+    @Test
+    public void testGetTeamMemberNegativeNotFound() throws Exception {
+        TeamMemberDto teamMemberDto = new TeamMemberDto();
+
+
+        when(ManagingTeamService.getTeamMember(1L, 1L)).thenReturn(teamMemberDto);
+
+        mockMvc.perform(get("/managing-team/{projectId}/", 1L, 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("currentUserId", "1"))
+                .andExpect(status().isNotFound());
+    }
 }
