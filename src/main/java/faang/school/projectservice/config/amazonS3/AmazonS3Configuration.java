86670fc5823779295242bc0fb3be37dfa7ae4c47
VovaBuzivskyi <vov4ikvseti@gmail.com>
2024-11-29 23:13:00 +0700

    set saving into corpbucket

--- src/main/java/faang/school/projectservice/config/amazonS3/AmazonS3Configuration.java
+++ src/main/java/faang/school/projectservice/config/amazonS3/AmazonS3Configuration.java
@@ -6,12 +6,10 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class AmazonS3Configuration {
 
@@ -27,23 +25,13 @@ public class AmazonS3Configuration {
     @Value("${services.s3.region}")
     private String region;
 
-    @Value("${services.s3.bucketName}")
-    private String bucketName;
-
     @Bean
     public AmazonS3 amazonS3() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(endpointUrl, region))
                 .build();
-
-        if (!amazonS3.doesBucketExistV2(bucketName)) {
-            amazonS3.createBucket(bucketName);
-            log.info("Bucket created: {}", bucketName);
-        }
-        return amazonS3;
     }
 }
