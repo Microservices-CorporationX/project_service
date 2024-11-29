976dd42fb856f22258180780d23c2e05b6a8b393
SerUKalinin <serukalinin@gmail.com>
2024-11-29 16:05:12 +0300

    Test

---
+++ src/test/java/faang/school/projectservice/controller/task/TaskControllerTest.java
@@ -0,0 +1,10 @@
+package faang.school.projectservice.controller.task;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TaskControllerTest {
+    @Test
+    void test() {
+    }
+}
---
+++ src/test/java/faang/school/projectservice/service/task/TaskServiceFilter.java
@@ -0,0 +1,9 @@
+package faang.school.projectservice.service.task;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskServiceFilter {
+    @Test
+    public void test() {
+    }
+}
