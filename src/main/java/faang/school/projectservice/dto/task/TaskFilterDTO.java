d98f2ceca4d9a777394360a55d5d9a2291493639
SerUKalinin <serukalinin@gmail.com>
2024-11-29 14:57:19 +0300

    TaskFilterDTO

---
+++ src/main/java/faang/school/projectservice/dto/task/TaskFilterDTO.java
@@ -0,0 +1,21 @@
+package faang.school.projectservice.dto.task;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskFilterDTO {
+    @Positive(message = "Идентификатор проекта не может быть отрицательным")
+    @NotNull(message = "Идентификатор проекта не может быть null")
+    private Long projectId;
+    private Long performerId;
+    private String status;
+    private String keyword;
+}
