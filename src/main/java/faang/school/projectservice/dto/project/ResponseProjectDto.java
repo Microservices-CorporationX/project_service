5bc64fc205a1948e11e00374a123331ebc41b88a
Vakarash <max.vackarash@gmail.com>
2024-11-29 14:44:55 +0300

    микроизменения дто

--- src/main/java/faang/school/projectservice/dto/project/ResponseProjectDto.java
+++ src/main/java/faang/school/projectservice/dto/project/ResponseProjectDto.java
@@ -1,16 +1,10 @@
 package faang.school.projectservice.dto.project;
 
-import faang.school.projectservice.model.ProjectStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
-import java.util.List;
-
 public record ResponseProjectDto(
         @Positive long id,
-        @NotBlank String name,
-        @NotBlank String description,
-        List<Long> teamIds,
-        ProjectStatus statuses
+        @NotBlank String name
 ) {
 }
