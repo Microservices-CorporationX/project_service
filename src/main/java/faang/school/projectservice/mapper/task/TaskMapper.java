bbe1bc8ba9611bebd92198791c9f6a8941cb6830
SerUKalinin <serukalinin@gmail.com>
2024-11-29 16:40:15 +0300

    TaskMapper

--- src/main/java/faang/school/projectservice/mapper/task/TaskMapper.java
+++ src/main/java/faang/school/projectservice/mapper/task/TaskMapper.java
@@ -3,13 +3,21 @@ package faang.school.projectservice.mapper.task;
 import faang.school.projectservice.dto.task.TaskDTO;
 import faang.school.projectservice.model.Task;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TaskMapper {
-    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
 
-    Task toEntity(TaskDTO taskDTO);
+    @Mapping(source = "project.id", target = "projectId")
+    @Mapping(source = "stage.id", target = "stageId")
+    @Mapping(source = "performerUser.id", target = "performerUserId")
+    @Mapping(source = "reporterUser.id", target = "reporterUserId")
+    Task toEntity(TaskDTO taskDto);
 
-    TaskDTO toDTO(Task task);
+    @Mapping(source = "projectId", target = "project.id")
+    @Mapping(source = "stageId", target = "stage.id")
+    @Mapping(source = "performerUserId", target = "performerUser.id")
+    @Mapping(source = "reporterUserId", target = "reporterUser.id")
+    TaskDTO toDto(Task task);
 }
