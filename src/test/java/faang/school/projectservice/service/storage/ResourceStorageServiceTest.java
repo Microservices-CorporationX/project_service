8621a7ac7debd89671ce057126c760b9900bbf4e
daurenberdibekov <berder08@gmail.com>
2024-11-30 12:11:07 +0100

    Fix resource storage service test

--- src/test/java/faang/school/projectservice/service/storage/ResourceStorageServiceTest.java
+++ src/test/java/faang/school/projectservice/service/storage/ResourceStorageServiceTest.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import faang.school.projectservice.config.s3.AwsS3Config;
 import faang.school.projectservice.model.Resource;
 import faang.school.projectservice.model.ResourceStatus;
+import faang.school.projectservice.model.ResourceType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,45 +21,36 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.anyString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yaml")
 @ActiveProfiles("test")
-class AwsS3ServiceTest {
+class ResourceStorageServiceTest {
     @MockBean
-    private AwsS3Config awsS3Client;
+    private AwsS3Config awsS3Config;
     @Autowired
     private ResourceStorageService awsS3Service;
+    @MockBean
+    private AmazonS3 amazonS3;
 
     @Value("${services.s3.bucketName}")
     private String bucketName;
 
     @Test
     void uploadResource() throws IOException {
-        MultipartFile file = mock(MultipartFile.class);
-        AmazonS3 s3Mock = mock(AmazonS3.class);
+        MultipartFile expectedResource = mock(MultipartFile.class);
 
-        when(file.getSize()).thenReturn(1024L);
-        when(file.getContentType()).thenReturn("image/png");
-        when(file.getOriginalFilename()).thenReturn("test.png");
-        when(file.getInputStream()).thenReturn(new ByteArrayInputStream(new byte[1024]));
-        when(awsS3Client.s3Config()).thenReturn(s3Mock);
+        when(expectedResource.getSize()).thenReturn(1024L);
+        when(expectedResource.getOriginalFilename()).thenReturn("test.png");
+        when(expectedResource.getInputStream()).thenReturn(new ByteArrayInputStream(new byte[1024]));
 
-        Resource resource = awsS3Service.uploadResource("test-folder", file);
+        Resource actualResource = awsS3Service.uploadResource("test-folder", expectedResource);
 
-        verify(s3Mock).putObject(eq(bucketName), anyString(), any(InputStream.class), any(ObjectMetadata.class));
-
-        assertNotNull(resource);
-        assertEquals("test.png", resource.getName());
-        assertEquals(ResourceStatus.ACTIVE, resource.getStatus());
+        assertNotNull(actualResource);
+        assertEquals(expectedResource.getOriginalFilename(), actualResource.getName());
+        assertEquals(ResourceStatus.ACTIVE, actualResource.getStatus());
     }
 
     @Test
@@ -66,15 +58,13 @@ class AwsS3ServiceTest {
         String key = "test-folder/20231126_test.png";
         S3Object s3ObjectMock = mock(S3Object.class);
         S3ObjectInputStream inputStreamMock = mock(S3ObjectInputStream.class);
-        AmazonS3 s3Mock = mock(AmazonS3.class);
 
         when(s3ObjectMock.getObjectContent()).thenReturn(inputStreamMock);
-        when(awsS3Client.s3Config()).thenReturn(s3Mock);
-        when(s3Mock.getObject(bucketName, key)).thenReturn(s3ObjectMock);
+        when(amazonS3.getObject(bucketName, key)).thenReturn(s3ObjectMock);
 
         InputStream result = awsS3Service.downloadResource(key);
 
-        verify(s3Mock).getObject(bucketName, key);
+        verify(amazonS3, times(1)).getObject(bucketName, key);
 
         assertNotNull(result);
     }
@@ -82,18 +72,16 @@ class AwsS3ServiceTest {
     @Test
     void updateResource() throws IOException {
         MultipartFile file = mock(MultipartFile.class);
-        AmazonS3 s3Mock = mock(AmazonS3.class);
         String key = "test-folder/20231126_test.png";
 
         when(file.getSize()).thenReturn(1024L);
-        when(file.getContentType()).thenReturn("image/png");
+        when(file.getContentType()).thenReturn(ResourceType.IMAGE.toString());
         when(file.getOriginalFilename()).thenReturn("updated.png");
         when(file.getInputStream()).thenReturn(new ByteArrayInputStream(new byte[1024]));
-        when(awsS3Client.s3Config()).thenReturn(s3Mock);
 
         Resource resource = awsS3Service.updateResource(key, file);
 
-        verify(s3Mock).putObject(eq(bucketName), eq(key), any(InputStream.class), any(ObjectMetadata.class));
+        verify(amazonS3, times(1)).putObject(eq(bucketName), eq(key), any(InputStream.class), any(ObjectMetadata.class));
 
         assertNotNull(resource);
         assertEquals("updated.png", resource.getName());
@@ -103,12 +91,9 @@ class AwsS3ServiceTest {
     @Test
     void deleteResource() {
         String key = "test-folder/20231126_test.png";
-        AmazonS3 s3Mock = mock(AmazonS3.class);
-
-        when(awsS3Client.s3Config()).thenReturn(s3Mock);
 
         awsS3Service.deleteResource(key);
 
-        verify(s3Mock).deleteObject(bucketName, key);
+        verify(amazonS3, times(1)).deleteObject(bucketName, key);
     }
 }
