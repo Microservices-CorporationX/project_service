b60c1bb3277c4c9f2c70303f6c99ae0d9bcbb026
SerUKalinin <serukalinin@gmail.com>
2024-12-02 11:47:58 +0300

    StatusFilter

--- src/main/java/faang/school/projectservice/service/task/filter/StatusFilter.java
+++ src/main/java/faang/school/projectservice/service/task/filter/StatusFilter.java
@@ -2,9 +2,9 @@ package faang.school.projectservice.service.task.filter;
 
 import faang.school.projectservice.dto.task.TaskFilterDTO;
 import faang.school.projectservice.model.Task;
+import faang.school.projectservice.model.TaskStatus;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 import java.util.stream.Stream;
 
 @Component
@@ -16,6 +16,7 @@ public class StatusFilter implements TaskFilter {
 
     @Override
     public Stream<Task> apply(Stream<Task> taskStream, TaskFilterDTO filterDTO) {
-        return taskStream.filter(task -> Objects.equals(task.getStatus(), filterDTO.getStatus()));
+        TaskStatus status = TaskStatus.valueOf(filterDTO.getStatus().toUpperCase());
+        return taskStream.filter(task -> task.getStatus() == status);
     }
 }
