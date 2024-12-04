ce346c2b55a79196a1ed7ee199778c8ff646a601
infani <abrormukimov@gmail.com>
2024-12-05 00:15:09 +0500

    Renamed s3config class

--- src/main/java/faang/school/projectservice/config/s3/AmazonS3Config.java
+++ src/main/java/faang/school/projectservice/config/s3/AmazonS3Config.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class S3Config {
+public class AmazonS3Config {
     @Value("${services.amazons3.accessKey}")
     private String accessKey;
 
