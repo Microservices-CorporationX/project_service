ae069cb61d18308a5d0f61810f5a6ba27d988778
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-12-04 19:35:28 +0300

    BJS2-41121 - context loads without credentials file

--- src/main/java/faang/school/projectservice/config/calendar/GoogleClientSecretsConfig.java
+++ src/main/java/faang/school/projectservice/config/calendar/GoogleClientSecretsConfig.java
@@ -2,6 +2,7 @@ package faang.school.projectservice.config.calendar;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.json.gson.GsonFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import java.io.InputStreamReader;
 
 
 @Configuration
+@Slf4j
 public class GoogleClientSecretsConfig {
     @Bean
     public GoogleClientSecrets googleClientSecrets() {
@@ -18,7 +20,8 @@ public class GoogleClientSecretsConfig {
             InputStream is = classloader.getResourceAsStream("credentials/google_credentials.json");
             return GoogleClientSecrets.load(GsonFactory.getDefaultInstance(), new InputStreamReader(is));
         } catch (Exception e) {
-            throw new IllegalStateException("Unable to load Google credentials from JSON", e);
+            log.error("Unable to load Google credentials from JSON");
+            return new GoogleClientSecrets();
         }
     }
 }
