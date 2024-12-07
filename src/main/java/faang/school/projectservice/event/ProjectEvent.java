e1f485b4186411e2006eb59a5cec88278f56c14a
VovaBuzivskyi <vov4ikvseti@gmail.com>
2024-12-07 22:33:21 +0700

    added: yaml in test, publish in projectService

--- src/main/java/faang/school/projectservice/event/ProjectEvent.java
+++ src/main/java/faang/school/projectservice/event/ProjectEvent.java
@@ -1,4 +1,17 @@
 package faang.school.projectservice.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectEvent {
+   private Long authorId;
+   private Long projectId;
 }
--- src/main/java/faang/school/projectservice/publisher/impl/ProjectEventPublisher.java
+++ src/main/java/faang/school/projectservice/publisher/impl/ProjectEventPublisher.java
@@ -3,10 +3,12 @@ package faang.school.projectservice.publisher.impl;
 import faang.school.projectservice.event.ProjectEvent;
 import faang.school.projectservice.publisher.MessagePublisher;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProjectEventPublisher implements MessagePublisher<ProjectEvent> {
@@ -19,5 +21,6 @@ public class ProjectEventPublisher implements MessagePublisher<ProjectEvent> {
     @Override
     public void publish(ProjectEvent message) {
         redisTemplate.convertAndSend(projectChannel, message);
+        log.info("Published project event - {}", message);
     }
 }
