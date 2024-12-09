a254cb696347b22f573f8edbb8bd4583b5495b86
VovaBuzivskyi <vov4ikvseti@gmail.com>
2024-12-09 23:38:29 +0700

    refactor: projectEvent

--- src/main/java/faang/school/projectservice/event/ProjectEvent.java
+++ src/main/java/faang/school/projectservice/event/ProjectEvent.java
@@ -1,17 +1,24 @@
 package faang.school.projectservice.event;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
 @Builder
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectEvent {
+
+   @NotNull
    private Long authorId;
+
+   @NotNull
    private Long projectId;
 }
