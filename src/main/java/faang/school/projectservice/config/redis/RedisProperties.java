561c5f53e8710176c5724acfc0c0a22697279a3a
AlekseyB86 <avbelov86@mail.ru>
2024-12-12 17:49:43 +0300

    настройка RedisConfig

---
+++ src/main/java/faang/school/projectservice/config/redis/RedisProperties.java
@@ -0,0 +1,11 @@
+package faang.school.projectservice.config.redis;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.redis")
+public record RedisProperties(
+        String host,
+        int port
+) {
+}
