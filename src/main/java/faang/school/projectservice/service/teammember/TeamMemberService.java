19c3200db028ce27fc38eb257e1dc3d101868328
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-12-21 14:00:50 +0300

    BJS2-41581 - Получение достижения "Менеджер"

--- src/main/java/faang/school/projectservice/service/teammember/TeamMemberService.java
+++ src/main/java/faang/school/projectservice/service/teammember/TeamMemberService.java
@@ -1,26 +1,9 @@
 package faang.school.projectservice.service.teammember;
 
-import faang.school.projectservice.exception.AccessDeniedException;
+
 import faang.school.projectservice.model.TeamMember;
-import faang.school.projectservice.repository.TeamMemberRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 public interface TeamMemberService {
     TeamMember findById(Long id);
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class TeamMemberService {
-    private final TeamMemberRepository teamMemberJpaRepository;
-
-    public TeamMember validateUserIsProjectMember(long userId, long projectId) {
-        TeamMember teamMember = teamMemberJpaRepository.findByUserIdAndProjectId(userId, projectId);
-        if (teamMember == null) {
-            log.info("User with id {} is not a member of project with id {}, access denied", userId, projectId);
-            throw new AccessDeniedException("Not a member of this project");
-        }
-        return teamMember;
-    }
+    TeamMember validateUserIsProjectMember(long userId, long projectId);
 }
