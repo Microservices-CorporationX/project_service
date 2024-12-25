7675783e8a875eff8331bb3a991d4d5342216231
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-12-25 12:09:34 +0100

    BJS2-41417 Анализ посещаемости профиля проекта, merge conflict fixes

--- src/main/java/faang/school/projectservice/publisher/ProjectViewEventPublisher.java
+++ src/main/java/faang/school/projectservice/publisher/ProjectViewEventPublisher.java
@@ -1,5 +1,6 @@
 package faang.school.projectservice.publisher;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.projectservice.events.ProjectViewEvent;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ProjectViewEventPublisher extends AbstractEventPublisher<ProjectViewEvent>{
-    public ProjectViewEventPublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic projectViewEventChannel) {
-        super(redisTemplate, projectViewEventChannel);
+    public ProjectViewEventPublisher(
+            RedisTemplate<String, Object> redisTemplate,
+            ChannelTopic projectViewEventChannel,
+            ObjectMapper objectMapper
+    ) {
+        super(redisTemplate, projectViewEventChannel, objectMapper);
     }
 
     @Override
