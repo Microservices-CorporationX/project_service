b14a43082336c2b4f8325d4d0b7fb122d66df558
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2025-01-07 13:44:39 +0500

    правки

--- src/main/java/faang/school/projectservice/validator/invitation/StageInvitationValidator.java
+++ src/main/java/faang/school/projectservice/validator/invitation/StageInvitationValidator.java
@@ -1,5 +1,7 @@
 package faang.school.projectservice.validator.invitation;
 
+import faang.school.projectservice.exception.StageInvitationException;
+import faang.school.projectservice.model.stage.Stage;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
 import faang.school.projectservice.model.stage_invitation.StageInvitationStatus;
 import org.springframework.stereotype.Component;
@@ -9,19 +11,19 @@ public class StageInvitationValidator {
 
     public void validatePendingStatus(StageInvitation stageInvitation) {
         if (!stageInvitation.getStatus().equals(StageInvitationStatus.PENDING)) {
-            throw new IllegalStateException("Invitation is not in a PENDING state");
+            throw new StageInvitationException("Invitation is not in a PENDING state");
         }
     }
 
     public void validateInvitedId(StageInvitation stageInvitation, Long invitedId) {
         if (!stageInvitation.getInvited().getId().equals(invitedId)) {
-            throw new IllegalArgumentException("Invited ID does not match the expected value");
+            throw new StageInvitationException("Invited ID does not match the expected value");
         }
     }
 
     public void validateRejectDescription(String rejectDescription) {
         if (rejectDescription == null && rejectDescription.isEmpty()) {
-            throw new IllegalArgumentException("description не может быть равен null или быть пустым");
+            throw new StageInvitationException("description не может быть равен null или быть пустым");
         }
     }
 }
