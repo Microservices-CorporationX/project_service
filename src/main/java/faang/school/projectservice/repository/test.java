4caddb49fc6da84d4ea211d141c2c2802537f3a1
Alexey Gorokhov <almigor@mail.ru>
2025-01-15 00:11:06 +0300

    bad code commit

---
+++ src/main/java/faang/school/projectservice/repository/test.java
@@ -0,0 +1,17 @@
+package faang.school.projectservice.repository;
+
+public class test {
+    public static void main(String[] args) {
+
+
+        System.out.println("Плохо отформатированный код");
+
+            for(int i = 0; i < 10; i++)
+            {
+                System.out.println(i);
+            }
+
+
+
+    }
+}
