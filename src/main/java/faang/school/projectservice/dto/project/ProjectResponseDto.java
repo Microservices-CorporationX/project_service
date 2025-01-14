66f4bfe795030ac37085d04e1550ce1780642f07
Stepan Evangulov <stevangulov@gmail.com>
2025-01-14 13:28:12 +0200

    Feature-BJS2-54033-Steph-Subprojects

--- src/main/java/faang/school/projectservice/dto/project/ProjectResponseDto.java
+++ src/main/java/faang/school/projectservice/dto/project/ProjectResponseDto.java
@@ -2,22 +2,21 @@ package faang.school.projectservice.dto.project;
 
 import faang.school.projectservice.model.ProjectStatus;
 import faang.school.projectservice.model.ProjectVisibility;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProjectResponseDto {
+public class ProjectDto {
     private Long id;
     private String name;
     private String description;
     private Long ownerId;
+    private Long parentProjectId;
+    private List<Long> childrenIds;
     private ProjectStatus status;
     private ProjectVisibility visibility;
     private LocalDateTime updatedAt;
