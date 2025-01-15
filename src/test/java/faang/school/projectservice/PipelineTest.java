fcdfa21f65899a8fb359576c42b007323d1b0b84
rostex <rostex2@yandex.ru>
2025-01-15 23:11:39 +0300

    Добавлен Тест для Workflow при создании PR (положительный результат)

--- src/test/java/faang/school/projectservice/PipelineTest.java
+++ src/test/java/faang/school/projectservice/PipelineTest.java
@@ -2,12 +2,13 @@ package faang.school.projectservice;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class PipelineTest {
 
     @Test
     public void testFailPipelineCase() {
-        fail();
+        assertTrue(true);
     }
 }
