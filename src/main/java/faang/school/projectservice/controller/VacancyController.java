200ad27a629ad7a584469bf5c5a14e99d2bc7b38
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-01-16 09:45:03 +0500

    intermediate commit: VacancyService, VacancyController

--- src/main/java/faang/school/projectservice/controller/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/VacancyController.java
@@ -1,8 +1,38 @@
 package faang.school.projectservice.controller;
 
+import faang.school.projectservice.dto.vacancy.VacancyDto;
+import faang.school.projectservice.mapper.VacancyMapper;
+import faang.school.projectservice.model.TeamRole;
+import faang.school.projectservice.model.Vacancy;
+import faang.school.projectservice.model.VacancyStatus;
+import faang.school.projectservice.service.VacancyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class VacancyController {
+import java.util.List;
 
+@Controller
+@RequiredArgsConstructor
+public class VacancyController {
+    private final VacancyService service;
+
+    public void createVacancy(VacancyDto vacancy) {
+        service.createVacancy(vacancy);
+    }
+
+    public void updateVacancy(VacancyDto vacancy) {
+        service.updateVacancy(vacancy);
+    }
+
+    public void removeVacancy(Long id) {
+        service.removeVacancy(id);
+    }
+
+    public List<Vacancy> filterByPosition(TeamRole role) {
+        return service.filterByPosition(role);
+    }
+
+    public List<Vacancy> filterByName(String str) {
+        return service.filterByName(str);
+    }
 }
--- src/main/java/faang/school/projectservice/service/VacancyService.java
+++ src/main/java/faang/school/projectservice/service/VacancyService.java
@@ -1,28 +1,45 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.dto.vacancy.VacancyDto;
+import faang.school.projectservice.mapper.VacancyMapper;
+import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.model.Vacancy;
 import faang.school.projectservice.repository.VacancyRepository;
 import faang.school.projectservice.validation.VacancyValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class VacancyService {
     private final VacancyRepository repository;
     private final VacancyValidator validator;
+    private final VacancyMapper mapper;
 
-    public void createVacancy(Vacancy vacancy) {
+    public void createVacancy(VacancyDto dto) {
+        Vacancy vacancy = mapper.toEntity(dto);
         validator.createValidate(vacancy);
         repository.save(vacancy);
     }
 
-    public void updateVacancy(Vacancy vacancy) {
+    public void updateVacancy(VacancyDto dto) {
+        Vacancy vacancy = mapper.toEntity(dto);
         validator.updateValidate(vacancy);
         repository.save(vacancy);
     }
+
     public void removeVacancy(Long id) {
         validator.removeValidate(id);
         repository.deleteById(id);
+    }
+
+    public List<Vacancy> filterByPosition(TeamRole role) {
+        return repository.findAll().stream().filter(vacancy -> vacancy.getPosition() == role).toList();
+    }
+
+    public List<Vacancy> filterByName(String str) {
+        return repository.findAll().stream().filter(vacancy -> vacancy.getName().contains(str)).toList();
     }
 }
