92936b9693928e5facbd18efcd8205a302e4e529
Dmitriy <dda20040609@gmail.com>
2025-01-17 11:42:04 +0300

    Commit dto

--- src/main/java/faang/school/projectservice/dto/stage/StageResponse.java
+++ src/main/java/faang/school/projectservice/dto/stage/StageResponse.java
@@ -1,5 +1,7 @@
 package faang.school.projectservice.dto.stage;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
@@ -7,8 +9,13 @@ import java.util.List;
 @Builder
 public record StageResponse(
         Long stageId,
-        String stageName,
-        Long projectId,
-        List<StageRolesDto> stageRolesDto
+        @NotBlank String stageName,
+        @NotNull Long projectId,
+        List<StageRolesDto> stageRolesDto,
+        String deletionStrategy,
+        Long targetStageId,
+        Long authorId,
+        List<String> requiredRoles,
+        List<Long> executorsIds
 ) {
 }
