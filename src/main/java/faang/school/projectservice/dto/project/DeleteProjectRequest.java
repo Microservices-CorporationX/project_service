908417f52b961a42cd52f35fbc466d2fd23b88d3
therealadik <vladerm2000@yandex.ru>
2025-01-17 12:22:16 +0500

    fix

--- src/main/java/faang/school/projectservice/dto/project/DeleteProjectRequest.java
+++ src/main/java/faang/school/projectservice/dto/project/DeleteProjectRequest.java
@@ -2,12 +2,11 @@ package faang.school.projectservice.dto.project;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 
 /**
  * DTO for {@link faang.school.projectservice.model.Project}
  */
 @Builder
-public record DeleteProjectRequest(@NotNull @Positive Long id, @NotNull @PositiveOrZero Long ownerId) {
+public record DeleteProjectRequest(@NotNull @Positive Long id, @NotNull @Positive Long ownerId) {
 }
