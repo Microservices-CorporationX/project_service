f31f767f3dedbf7a443b2cd67828165adde95c35
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2025-01-19 01:11:10 +0300

    fail test

--- src/test/java/faang/school/projectservice/ProjectServiceApplicationTest.java
+++ src/test/java/faang/school/projectservice/ProjectServiceApplicationTest.java
@@ -2,10 +2,12 @@ package faang.school.projectservice;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 class ProjectServiceApplicationTest {
 
     @Test
     void testMainFail() {
-
+        fail();
     }
 }
