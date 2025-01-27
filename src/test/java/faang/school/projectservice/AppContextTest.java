144d38d8eede17b349fe506f53c144676ea39e4a
Aleksandr Kozlov <vismut-888@yandex.ru>
2025-01-27 17:02:35 +0300

    fix CI

--- src/test/java/faang/school/projectservice/AppContextTest.java
+++ src/test/java/faang/school/projectservice/AppContextTest.java
@@ -6,14 +6,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class AppContextTest {
     @Autowired
     private ProjectServiceApplication projectServiceApplication;
+
+    @Autowired
+    private DataSource dataSource;
 
     private static final PostgreSQLContainer<?> postgresContainer;
 
@@ -21,7 +29,8 @@ public class AppContextTest {
         postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13.3"))
                 .withCreateContainerCmdModifier(cmd -> cmd.withName("testDb"))
                 .withUsername("user")
-                .withPassword("password");
+                .withPassword("password")
+                .waitingFor(Wait.forListeningPort());
     }
 
     @BeforeAll
@@ -32,6 +41,12 @@ public class AppContextTest {
     @Test
     public void contextLoads() {
         assertNotNull(projectServiceApplication);
+
+        try (Connection connection = dataSource.getConnection()) {
+            assertNotNull(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterAll
