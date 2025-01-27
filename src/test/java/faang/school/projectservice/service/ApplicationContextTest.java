92278595d427f1e019b0e6c336cb9d447cfbcaf3
triplek <weribel@mail.ru>
2025-01-27 22:53:07 +0300

    Задача "Стажировки" без тестов, исправил после замечаний на кросс-ревью

--- src/test/java/faang/school/projectservice/service/ApplicationContextTest.java
+++ src/test/java/faang/school/projectservice/service/ApplicationContextTest.java
@@ -1,7 +1,5 @@
 package faang.school.projectservice.service;
 
-package com.example;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
