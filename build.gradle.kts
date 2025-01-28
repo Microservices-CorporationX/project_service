caf0e28dbb2d08c1cd8bafb238de2a1175c3a4a7
triplek <weribel@mail.ru>
2025-01-28 13:51:11 +0300

    попытка тестирование поднятия Spring context

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,10 @@ repositories {
 
 dependencies {
     /**
-    * Spring boot starters
-    */
+     * Spring boot starters
+     */
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
@@ -54,10 +55,9 @@ dependencies {
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
 
-
     /**
-    * Tests
-    */
+     * Tests
+     */
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
