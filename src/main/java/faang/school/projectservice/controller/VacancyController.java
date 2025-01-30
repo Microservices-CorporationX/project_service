f3515a9be99cc2e3a93db43dcb91b70396278702
OxanaNovozhilova <nov2303@mail.ru>
2025-01-30 13:56:24 +0500

    BJS2-53001 | Добавление обложки вакансии

--- src/main/java/faang/school/projectservice/controller/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/VacancyController.java
@@ -3,6 +3,7 @@ package faang.school.projectservice.controller;
 import faang.school.projectservice.service.VacancyService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/v1/vacancy")
 @RequiredArgsConstructor
+@Slf4j
 public class VacancyController {
     VacancyService vacancyService;
 
@@ -26,7 +28,10 @@ public class VacancyController {
 
     @PutMapping("/{vacancyId}/add")
     public void addCover(@PathVariable Long vacancyId, @RequestBody MultipartFile file) {
-
+//        Ограничение на размер файла — не более 5 МБ.
+//        WHere this check?
+//        Удаление доступно только автору вакансии или владельцу/менеджеру проекта.
+//        Изображение должно быть ужато так, чтобы самая большая сторона не превышала 512 пикселей перед сохранением.
     }
 
 }
--- src/main/java/faang/school/projectservice/service/VacancyServiceImpl.java
+++ src/main/java/faang/school/projectservice/service/VacancyServiceImpl.java
@@ -1,5 +1,8 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.adapter.VacancyRepositoryAdapter;
+import faang.school.projectservice.model.Vacancy;
+import faang.school.projectservice.repository.VacancyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,9 +10,13 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class VacancyServiceImpl implements VacancyService{
+    private final VacancyRepositoryAdapter vacancyRepositoryAdapter;
 
     @Override
     public void addCover(Long vacancyId, MultipartFile file) {
+        Vacancy vacancy = vacancyRepositoryAdapter.findById(vacancyId);
+        vacancy.setCoverImageKey();
+
 
     }
 }
