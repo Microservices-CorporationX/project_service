cf4123b84c4da73eb8258450e81a4083470953de
therealadik <53974452+therealadik@users.noreply.github.com>
2025-02-02 14:22:35 +0500

    Revert "Задача "Стажировки" без тестов"

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,14 +55,13 @@ dependencies {
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
 
+
     /**
     * Tests
     */
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation ("org.testcontainers:testcontainers")
-    testImplementation ("org.testcontainers:postgresql")
 
     /**
      *  Swagger UI & OpenAI
