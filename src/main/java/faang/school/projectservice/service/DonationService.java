8a0111cfb089b4160aeceb06350ca699cd1db9b8
Ilya Vysokolyan <rd2d@list.ru>
2025-02-03 21:25:21 +0300

    BJS2-54267: Исправил UserService

--- src/main/java/faang/school/projectservice/service/DonationService.java
+++ src/main/java/faang/school/projectservice/service/DonationService.java
@@ -1,12 +1,12 @@
 package faang.school.projectservice.service;
 
 import faang.school.projectservice.client.PaymentServiceClient;
-import faang.school.projectservice.client.UserServiceClient;
 import faang.school.projectservice.dto.client.PaymentRequest;
 import faang.school.projectservice.dto.donation.DonationDto;
 import faang.school.projectservice.dto.donation.DonationFilter;
 import faang.school.projectservice.exception.BusinessException;
 import faang.school.projectservice.exception.DataValidationException;
+import faang.school.projectservice.exception.EntityNotFoundException;
 import faang.school.projectservice.spetification.DonationSpecification;
 import faang.school.projectservice.mapper.DonationMapper;
 import faang.school.projectservice.model.Campaign;
@@ -26,7 +26,7 @@ public class DonationService {
     private final DonationMapper donationMapper;
     private final PaymentServiceClient paymentServiceClient;
     private final CampaignService campaignService;
-    private final UserServiceClient userServiceClient;
+    private final UserService userService;
     private final DonationSpecification donationSpecification;
 
 
@@ -42,13 +42,14 @@ public class DonationService {
         if (id == null || userId == null) {
             throw new DataValidationException("Ошибка валидации метода findDonationByIdAndUserId");
         }
-        //findUserById(userId);
-        var donation = donationRepository.findByIdAndUserId(id, userId).orElseThrow();
+        findUserById(userId);
+        var donation = donationRepository.findByIdAndUserId(id, userId)
+                .orElseThrow(() -> new EntityNotFoundException("Донат с ID " + id + " не найден "));
         return donationMapper.toDto(donation);
     }
 
     public List<DonationDto> getDonationByIdUser(long userId, DonationFilter donationFilter) {
-        //findUserById(userId);
+        findUserById(userId);
         Specification<Donation> donationSpec = donationSpecification.build(userId, donationFilter);
         List<Donation> donations = donationRepository.findAll(donationSpec);
         return donations.stream().map(donationMapper::toDto).toList();
@@ -76,7 +77,7 @@ public class DonationService {
     }
 
     private void findUserById(long userId) {
-        userServiceClient.getUser(userId);
+        userService.getUserDtoById(userId);
     }
 
 }
