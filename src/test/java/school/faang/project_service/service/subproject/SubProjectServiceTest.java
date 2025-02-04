87db7653c02d682e7380aae4a3a724fb2625533e
Anton Graf <ant0ngraf@yandex.ru>
2025-02-04 12:45:15 +0600

    fix conflicts

--- src/test/java/school/faang/project_service/service/subproject/SubProjectServiceTest.java
+++ src/test/java/school/faang/project_service/service/subproject/SubProjectServiceTest.java
@@ -1,4 +1,4 @@
-package faang.school.projectservice.service.subproject;
+package school.faang.project_service.service.subproject;
 
 import faang.school.projectservice.dto.subproject.SubProjectFilterDto;
 import faang.school.projectservice.exeption.ProjectNotClosableException;
